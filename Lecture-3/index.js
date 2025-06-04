d4d71e5edae06397d8311c1e1d7e36f4bca37122
vasu-636 <vasupatel632006@gmail.com>
2025-06-04 10:34:08 +0530

    operator practice done

---
+++ Lecture-3/index.js
@@ -0,0 +1,64 @@
+let a = 3 ;
+let b = 6 ;
+
+console.log("Arithmatic Operations");
+console.log(a +b);
+console.log(a-b);
+console.log(a*b);
+console.log(b/a);
+console.log(b % a);
+console.log(b ** a);
+console.log("Comparison Operations");
+console.log(a == b);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log("Logical Operations");
+console.log(a == b && a < b);
+console.log(a == b || a < b);
+console.log(!(a == b));
+console.log("Bitwise Operations");
+console.log(a & b);
+console.log(a | b);
+console.log(a ^ b);
+console.log(~a);
+
+console.log(a << 1);
+console.log(a >> 1);
+console.log("Assignment Operations");
+console.log(a += b);
+
+console.log(a -= b);
+console.log(a *= b);
+
+console.log(a /= b);
+console.log(a %= b);
+console.log(a **= b);
+console.log(a <<= 1);
+console.log(a >>= 1);
+console.log(a &= b);
+console.log(a |= b);
+
+console.log(a ^= b);
+
+console.log("Identity Operations");
+console.log(a === b);
+console.log(a !== b);
+
+console.log("Typeof Operations");
+
+console.log(typeof a);
+console.log(typeof b);
+
+console.log("Instanceof Operations");
+console.log(a instanceof Number);
+
+
+
+
+
+
+
+
