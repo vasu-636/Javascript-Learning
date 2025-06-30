0a6d89584985cbc4f4a6b029c593c2b92594f46b
vasu-636 <vasupatel632006@gmail.com>
2025-06-30 10:22:12 +0530

    function completed

---
+++ Lecture-11-function/script.js
@@ -0,0 +1,15 @@
+let choice = 1;
+
+switch(choice){
+    case 1: 
+        console.log("You chose option 1");
+        break;
+    case 2:
+        console.log("You chose option 2");
+        break;
+    case 3:
+        console.log("You chose option 3");
+        break;
+    default:
+        console.log("Invalid choice");
+    }
